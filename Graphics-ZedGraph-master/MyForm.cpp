854f743a5db7b6e62c30cc1544c86c0445906e84
DanyaLitva <140255125+DanyaLitva@users.noreply.github.com>
2025-10-16 19:03:56 +0300

    russian

--- Graphics-ZedGraph-master/MyForm.cpp
+++ Graphics-ZedGraph-master/MyForm.cpp
@@ -7,6 +7,8 @@ using namespace System::Windows::Forms;
 [STAThread]
 void Main(array<String^>^ args)
 {
+	setlocale(LC_ALL, "Russian");
+	//system("chcp 1251");
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
